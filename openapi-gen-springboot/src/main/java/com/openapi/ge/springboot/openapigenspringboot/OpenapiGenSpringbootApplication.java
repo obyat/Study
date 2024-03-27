83319a0561a8b0d0954f909f44bd159501556e26
Obyat <rahman.obyat@gmail.com>
2024-03-26 23:29:36 -0400

    [OpenApi] add boilerplate code for openapi

---
+++ openapi-gen-springboot/src/main/java/com/openapi/ge/springboot/openapigenspringboot/OpenapiGenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.openapi.ge.springboot.openapigenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiGenSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiGenSpringbootApplication.class, args);
+	}
+
+}
---
+++ openapi-gen-springboot/src/test/java/com/openapi/ge/springboot/openapigenspringboot/OpenapiGenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.ge.springboot.openapigenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGenSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
